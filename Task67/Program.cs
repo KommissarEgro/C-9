b512d1785bcd16ebdd9803df944477eb1c1e1ea7
Kommissar Egro <ugalovnik666@gmil.com>
2023-09-09 15:42:42 +1000

    решение задачи 67

--- Task67/Program.cs
+++ Task67/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("Введит целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigits(int number) //453 -> 45 -> 4
+{
+    if (number == 0) return 0;
+    return number % 10 + SumDigits(number / 10); // 453, 45 --> 4 --> 0
+                                                 // 0 + 4 % 10 + 45 % 10 + 453 % 10
+}
+
+
+Console.WriteLine(SumDigits(number));
+
