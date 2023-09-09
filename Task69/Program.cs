de944e49b4baa078dfc961d8a818eaa58d6c1ca8
Kommissar Egro <ugalovnik666@gmil.com>
2023-09-09 16:04:26 +1000

    решение задачи 69

--- Task69/Program.cs
+++ Task69/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.WriteLine("Введит первое целое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введит второе натуральное число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberB < 0) {
+Console.WriteLine("Это число не подходит, введите число большее или равное 0");
+return;
+}
+int PowNum(int numA, int numB) 
+{
+    if (numB == 0) return 1;
+    return numA * PowNum(numA,numB -1);
+}
+int powNum = PowNum(numberA, numberB);
+Console.WriteLine(powNum);
